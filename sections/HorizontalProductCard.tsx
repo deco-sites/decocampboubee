50c450a9d03f5c11f8f7d5cda33cf4043ee4cba8
gabrielMatosBoubee <gm.boubee@gmail.com>
2024-04-21 16:47:05 -0300

    feat: add HorizontalProductCard

--- sections/HorizontalProductCard.tsx
+++ sections/HorizontalProductCard.tsx
@@ -5,16 +5,10 @@ import { mapProductToAnalyticsItem } from "apps/commerce/utils/productToAnalytic
 import Image from "apps/website/components/Image.tsx";
 import type { Platform } from "../apps/site.ts";
 import { SendEventOnClick } from "../components/Analytics.tsx";
-import Avatar from "../components/ui/Avatar.tsx";
-import {
-  default as WishlistButtonVtex,
-  default as WishlistButtonWake,
-} from "../islands/WishlistButton/vtex.tsx";
-import { clx } from "../sdk/clx.ts";
+import AddToCartButton from "../islands/AddToCartButton/vtex.tsx";
 import { formatPrice } from "../sdk/format.ts";
 import { relative } from "../sdk/url.ts";
 import { useOffer } from "../sdk/useOffer.ts";
-import { useVariantPossibilities } from "../sdk/useVariantPossiblities.ts";
 
 interface Props {
   product: Product;
@@ -31,32 +25,45 @@ interface Props {
 }
 
 const WIDTH = 200;
-const HEIGHT = 279;
+const HEIGHT = 150;
 
 function HorizontalProductCard({
   product,
   preload,
   itemListName,
-  platform,
   index,
 }: Props) {
   const { url, productID, name, image: images, offers, isVariantOf } = product;
   const id = `product-card-${productID}`;
-  const hasVariant = isVariantOf?.hasVariant ?? [];
-  const productGroupID = isVariantOf?.productGroupID;
   const description = product.description || isVariantOf?.description;
-  const [front, back] = images ?? [];
-  const { listPrice, price, installments } = useOffer(offers);
-  const possibilities = useVariantPossibilities(hasVariant, product);
-  const variants = Object.entries(Object.values(possibilities)[0] ?? {});
+  const [front] = images ?? [];
+  const { listPrice, price, installments, seller = "1" } = useOffer(offers);
   const relativeUrl = relative(url);
   const aspectRatio = `${WIDTH} / ${HEIGHT}`;
+
+
+
+  const eventItem = mapProductToAnalyticsItem({
+    product,
+    breadcrumbList:{ 
+        "@type": "BreadcrumbList",
+        "itemListElement": [{
+        "@type": "ListItem",
+        "name": product!.name,
+        "item": product!.url ?? "/",
+        "position": 1,
+        }],
+        "numberOfItems": 1
+        },
+    price,
+    listPrice,
+  });
 
   return (
     <div
       id={id}
       data-deco="view-product"
-      class="card card-compact group w-full lg:border lg:border-transparent lg:hover:border-inherit lg:p-4"
+      class="card card-compact group w-full border lg:p-4"
     >
       {/* Add click event to dataLayer */}
       <SendEventOnClick
@@ -77,120 +84,57 @@ function HorizontalProductCard({
         }}
       />
 
-      <div class="flex flex-row gap-2 lg:group-hover:-translate-y-2">
+      <div class="flex flex-row flex-wrap items-start max-md:justify-center md:justify-between rounded-xl min-h-52 gap-4 p-4">
+        <div class="flex justify-start gap-4">
         <figure
           class="relative overflow-hidden"
-          style={{ aspectRatio }}
+          style={{ aspectRatio, width: WIDTH, height: HEIGHT }}
         >
-          {/* Wishlist button */}
-          <div
-            class={clx(
-              "absolute top-0 left-0",
-              "z-10 w-full",
-              "flex items-center justify-end",
-            )}
-          >
-            {/* Discount % */}
-            <div class="text-sm px-3">
-              <span class="font-bold">
-                {listPrice && price
-                  ? `${Math.round(((listPrice - price) / listPrice) * 100)}% `
-                  : ""}
-              </span>
-              OFF
-            </div>
-            <div class="lg:group-hover:block">
-              {platform === "vtex" && (
-                <WishlistButtonVtex
-                  productGroupID={productGroupID}
-                  productID={productID}
-                />
-              )}
-              {platform === "wake" && (
-                <WishlistButtonWake
-                  productGroupID={productGroupID}
-                  productID={productID}
-                />
-              )}
-            </div>
-          </div>
-
           {/* Product Images */}
           <a
             href={relativeUrl}
             aria-label="view product"
-            class={clx(
-              "absolute top-0 left-0",
-              "grid grid-cols-1 grid-rows-1",
-              "w-full",
-            )}
           >
             <Image
               src={front.url!}
               alt={front.alternateName}
+              class="rounded-lg"
               width={WIDTH}
               height={HEIGHT}
               style={{ aspectRatio }}
-              class={clx(
-                "bg-base-100",
-                "object-cover",
-                "rounded w-full",
-                "col-span-full row-span-full",
-              )}
               sizes="(max-width: 640px) 50vw, 20vw"
               preload={preload}
               loading={preload ? "eager" : "lazy"}
               decoding="async"
             />
-            <Image
-              src={back?.url ?? front.url!}
-              alt={back?.alternateName ?? front.alternateName}
-              width={WIDTH}
-              height={HEIGHT}
-              style={{ aspectRatio }}
-              class={clx(
-                "bg-base-100",
-                "object-cover",
-                "rounded w-full",
-                "col-span-full row-span-full",
-                "transition-opacity opacity-0 lg:group-hover:opacity-100",
-              )}
-              sizes="(max-width: 640px) 50vw, 20vw"
-              loading="lazy"
-              decoding="async"
-            />
           </a>
         </figure>
 
-        {/* SKU Selector */}
-        <ul class="flex items-center justify-center gap-2">
-          {variants
-            .map(([value, link]) => [value, relative(link)] as const)
-            .map(([value, link]) => (
-              <li>
-                <a href={link}>
-                  <Avatar
-                    content={value}
-                    variant={link === relativeUrl
-                      ? "active"
-                      : link
-                      ? "default"
-                      : "disabled"}
-                  />
-                </a>
-              </li>
-            ))}
-        </ul>
-
         {/* Name/Description */}
-        <div class="flex flex-col">
+        <div class="flex flex-col max-md:hidden">
           <h2
-            class="truncate text-base lg:text-lg uppercase"
+            class="text-base lg:text-lg uppercase"
             dangerouslySetInnerHTML={{ __html: name ?? "" }}
           />
 
           <div
-            class="truncate text-xs"
+            class="text-xs line-clamp-2"
+            dangerouslySetInnerHTML={{ __html: description ?? "" }}
+          />
+        </div>
+        
+
+        </div>
+        <div class="flex flex-col justify-between px-4 md:border-l min-h-52 h-full">
+        {/* Name/Description */}
+        <div class="flex flex-col md:hidden">
+          <h2
+            class="text-base lg:text-lg uppercase"
+            dangerouslySetInnerHTML={{ __html: name ?? "" }}
+          />
+
+          <div
+            class="text-xs line-clamp-2"
             dangerouslySetInnerHTML={{ __html: description ?? "" }}
           />
         </div>
@@ -209,14 +153,12 @@ function HorizontalProductCard({
         <span class="flex justify-end gap-2 font-light text-sm truncate">
           ou {installments}
         </span>
-
-        <a
-          href={relativeUrl}
-          aria-label="view product"
-          class="btn btn-block"
-        >
-          Ver produto
-        </a>
+        <AddToCartButton 
+        eventParams={{ items: [eventItem] }}
+                    productID={productID}
+                    seller={seller} 
+                    />
+        </div>
       </div>
     </div>
   );
